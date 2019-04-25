412b42c87baf8600a60701c5720d38b64db6dc3c
iqbal <asifmai@hotmail.com>
2019-04-25 16:29:27 +0500

    'update'

--- controllers/indexcontroller.js
+++ controllers/indexcontroller.js
@@ -1,3 +1,10 @@
+// Show index page
 module.exports.index_get = (req, res, next) => {
-  res.render('index');
+  res.render('index', {page: req.url});
+}
+
+// Show Choose Waiver Page
+module.exports.choosewaiver_post = (req, res, next) => {
+  res.locals.guestemail = req.body.email;
+  res.render('choosewaiver');
 }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ const indexController = require('../controllers/indexcontroller');
 /* GET home page. */
 router.get('/', indexController.index_get);
 
+/* GET home page. */
+router.post('/choosewaiver', indexController.choosewaiver_post);
+
 module.exports = router;
